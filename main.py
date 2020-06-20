5ab2198127e4a382b213a7a0a0ede2cf957970ed
Encrypted Message <encryptemessage@protonmail.ch>
2020-06-20 22:42:59 +0200

    Add regex to ensure it doesn't reply to posts that are anti-vpn

--- main.py
+++ main.py
@@ -10,31 +10,38 @@ reddit = praw.Reddit('autoBot')
 
 #Load in all replied to posts, not efficent my ass, make it better if you cant live with it. 
 if not os.path.isfile("posts_replied_to.txt"):
-	posts_replied_to = []
+    posts_replied_to = []
 else:
-	with open("posts_replied_to.txt", "r") as f:
-		posts_replied_to = f.read()
-		posts_replied_to = posts_replied_to.split("\n")
-		posts_replied_to = list(filter(None, posts_replied_to)) #I don't expect anyone to ask "why list()?", but to anyone that does: pyyyyyyython3!
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+	posts_replied_to = posts_replied_to.split("\n")
+	posts_replied_to = list(filter(None, posts_replied_to))
 
 #Point the bot at r/TOR
-subreddit = reddit.subreddit('pythonforengineers')
+subreddit = reddit.subreddit('TOR')
 
 #Read the 5 newest posts (luckily r/TOR isnt that active)
-for submission in subreddit.new(limit=1):
-	#Check to ensure we don't spam a post
-        if submission.id not in posts_replied_to:
-            if re.search("vpn", submission.selftext, re.IGNORECASE): 
-                    #FUCK. I need to find a way to ensure it anwsers to only the correct posts, but this is only v0.1, and I am going to need some devs that know best pratices.
-                
-                #Put everything in a file to make this code just a little more readable, not efficent my ass, if you know how to do it bette, well then do it!
+for submission in subreddit.new(limit=5):
+    
+    #Check to ensure we don't spam a post
+    if submission.id not in posts_replied_to:
+        
+        if re.search("vpn", submission.selftext, re.IGNORECASE):
+                #RE is regex, anyone that knows regex please help create a string that correctly identifies posts.
+
+                #Check for markers for anti-vpn (don't want to add a comment saying VPN bad to someone saying they aren't using vpn)
+            if re.search("\bdon(\')*t use( a)* vpn\b|\bdidn(\')*t use( a)* vpn\b|\bnot using( a)* vpn\b|will not( be)* us(e|ing)( a)* vpn")
+            
+            #Put everything in a file to make this code just a little more readable.
                 with open("reply.txt", "r") as f:
+                    
                     #Now reply
                     submission.reply(f.read())
-                print("Bot replying to: ", submission.title)
+            
+            print("Bot replying to: ", submission.title)
 
-                #Add the post to our list
-                posts_replied_to.append(submission.id)
+            #Add the post to our list
+            posts_replied_to.append(submission.id)
 
 #Overwrite the posts_replied_to.txt with current list
 with open("posts_replied_to.txt", "w") as f:
